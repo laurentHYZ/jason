d4acaca5e7e59e9d44f1b0626192aa206fce8931
laurent <laurent_pinson@hotmail.com>
2020-07-08 18:31:47 +0700

    asdf

---
+++ hello.py
@@ -0,0 +1,14 @@
+# you need to declare the hello and byebye variables
+# you will print hello and print byebye
+# when we pick the name of the box, we usually don;'t use capital letters
+# we do not start with a number
+# we usually do not start with any special signs (*?# etc )
+hello = 'Hello and welcome to my dream land'
+user_name = input("What is your name, young hero? ")
+if len(user_name) > 3:
+    print("you are welcome here")
+else:
+    print("your name is too short, GEEEEET OOOOOUUUUTTT WWWHHHIIILLLEEE YYYOOOUUU SSSTTTIIILLLLLL CCCAAANNN!!!!!" + user_name +"!!!")
+    
+
+
