4c8795124a3551545f6c246273860ce6eb0aa06c
laurent <laurent_pinson@hotmail.com>
2020-07-11 16:11:13 +0700

    added rachel

--- hello.py
+++ hello.py
@@ -3,12 +3,27 @@
 # when we pick the name of the box, we usually don;'t use capital letters
 # we do not start with a number
 # we usually do not start with any special signs (*?# etc )
-hello = 'Hello and welcome to my dream land'
-user_name = input("What is your name, young hero? ")
-if len(user_name) > 3:
-    print("you are welcome here")
-else:
-    print("your name is too short, GEEEEET OOOOOUUUUTTT WWWHHHIIILLLEEE YYYOOOUUU SSSTTTIIILLLLLL CCCAAANNN!!!!!" + user_name +"!!!")
-    
+info_about_jason = ["jason chengyu", 'gaming', 'brocoli']
+for letter in "jason":
+    print(letter)
+# please write a "for loop" (the very same loop as the "for letter in "jason")
+# and make python print each element within info_about_jason
+
+for item in info_about_jason:
+    print(item)
+
+print(info_about_jason[2])
+info_about_jason.append("")
+
+# I am going to ___ the s___ "beijing" to my __ info_about_jason
+info_about_jason.append("beijing")
+# I am going to ___ the inte___ 2 to my ___ info_about_jason
+info_about_jason.append(2)
+# I am going to ___ the f___ 2.2 to my ___ info_about_jason
+info_about_jason.append(2.2)
+# I am going to ___ the b___ True to my ___ info_about_jason #boolean
+info_about_jason.append(T)
+info_about_jason.append(1)
 
 
+len(info_about_jason)
