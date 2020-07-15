136a06898850a80454158cf5bfed3d102fa761dd
laurent <laurent_pinson@hotmail.com>
2020-07-15 15:16:02 +0700

    asdf

--- hello.py
+++ hello.py
@@ -4,26 +4,7 @@
 # we do not start with a number
 # we usually do not start with any special signs (*?# etc )
 info_about_jason = ["jason chengyu", 'gaming', 'brocoli']
-for letter in "jason":
-    print(letter)
-# please write a "for loop" (the very same loop as the "for letter in "jason")
-# and make python print each element within info_about_jason
-
-for item in info_about_jason:
-    print(item)
-
-print(info_about_jason[2])
-info_about_jason.append("")
-
-# I am going to ___ the s___ "beijing" to my __ info_about_jason
-info_about_jason.append("beijing")
-# I am going to ___ the inte___ 2 to my ___ info_about_jason
-info_about_jason.append(2)
-# I am going to ___ the f___ 2.2 to my ___ info_about_jason
-info_about_jason.append(2.2)
-# I am going to ___ the b___ True to my ___ info_about_jason #boolean
-info_about_jason.append(T)
-info_about_jason.append(1)
-
-
-len(info_about_jason)
+info_about_jason.append('chinese')
+info_about_jason.append(11)
+everything is an object  (something that has attributes and capabilities/superpowers)
+# jason.write('hello)
