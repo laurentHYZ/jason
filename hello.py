4dad35b7b993a1ec2884ebc770183616a76042f8
laurent <laurent_pinson@hotmail.com>
2020-07-20 16:01:33 +0700

    asdf

--- hello.py
+++ hello.py
@@ -1,10 +1,35 @@
-# you need to declare the hello and byebye variables
-# you will print hello and print byebye
-# when we pick the name of the box, we usually don;'t use capital letters
-# we do not start with a number
-# we usually do not start with any special signs (*?# etc )
-info_about_jason = ["jason chengyu", 'gaming', 'brocoli']
-info_about_jason.append('chinese')
-info_about_jason.append(11)
-everything is an object  (something that has attributes and capabilities/superpowers)
-# jason.write('hello)
+
+# jason.write()
+You Jason, write!
+# Walk fast !
+walk(fast)
+# Come ! 
+come()
+# Write()
+write()
+# write(“hello”)
+Write “hello”!!
+# Say “Hi !
+say(“Hi”)
+# Say “thank you Dad!” !
+say(“thank you Dad!”)
+# say(“pleased to meet you.”)
+Say "pleased to meet you"!
+# Mike’s bike
+mike.bike
+# The bike of Mike
+# Mike.bike
+# The dad of Mike 
+mike.dad
+# Mike’s dad
+# jason.mother
+Jason's mother
+# The age of Mike
+# Mike.age
+# The age of mike is 11
+mike.age = 11
+# Mike.age = 11
+# The age of jason 
+# jason.mother.age
+# jason.mother.age = 30
+Jason's mother's age is 30.
